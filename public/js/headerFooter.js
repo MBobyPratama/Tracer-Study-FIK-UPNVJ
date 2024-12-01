71422d0ba298e905e3f2ade63a6ddee7c9d10984
Muhammad Boby Pratama <bobyjakarta4@gmail.com>
2024-12-02 01:21:06 +0700

    first  commit

---
+++ public/js/headerFooter.js
@@ -0,0 +1,76 @@
+class SpecialHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+      <header>
+    <img class="logo" src="images/logo-fik.png">
+    <nav class="navbar">
+      <a href="/home">Beranda</a>
+      <a href="#">Tentang Kami</a>
+      <a href="#">Berita</a>
+      
+      <div class="menu-item">
+        <a href="#">Alumni ▾</a>
+        <div class="dropdown">
+          <a href="#">Tracer Study FIK UPNVJ</a>
+        </div>
+      </div>
+
+      <div class="menu-item">
+        <a href="#">Karier ▾</a>
+        <div class="dropdown">
+          <a href="#">Lowongan Kerja</a>
+          <a href="#">Magang</a>
+        </div>
+      </div>
+
+      <div class="menu-item">
+        <a href="#">Event ▾</a>
+        <div class="dropdown">
+          <a href="#">Career Class</a>
+          <a href="#">Company Talk</a>
+          <a href="#">Career Fair</a>
+          <a href="#">Alumni Sharing</a>
+        </div>
+      </div>
+
+      <a href="#" class="btn-login-link">
+        <button class="btn-login">Login Alumni</button>
+      </a>
+    </nav>
+  </header>
+    `;
+    }
+}
+
+class SpecialFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+      <footer>
+    <div class="container">
+      <img class="logo-upn" src="images/logo-fik.png">
+      <h1>FIK - UPNVJ</h1>
+      <div class="contact-info">
+        <div class="contact-item">
+          <i class="fa-solid fa-map-location-dot"></i>
+          <p>Jl. RS. Fatmawati, Pondok Labu, Jakarta Selatan, 12450</p>
+        </div>
+        <div class="contact-item">
+          <i class="fa-solid fa-envelope"></i>
+          <a href="tel:+62217656971">(021)7656971</a>
+        </div>
+        <div class="contact-item">
+          <i class="fa-solid fa-phone"></i>
+          <a href="mailto:upnvj@upnvj.ac.id">upnvj@upnvj.ac.id</a>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      Copyright ©2024 Fakultas Ilmu Komputer UPN Veteran Jakarta<br>
+    </div>
+  </footer>
+    `;
+    }
+}
+
+customElements.define("special-header", SpecialHeader);
+customElements.define("special-footer", SpecialFooter);
---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tracer Study FIK UPNVJ</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  {{-- <script src="js/script.js"></script> --}}
+</head>
+
+<body>
+  <special-header></special-header>
+
+  <section class="hero">
+      <div class="container">
+        <div class="home">
+          <div class="banner-card">
+            <img class="image-about" src="images/background2.png" alt="background2">
+            <div class="banner-text">
+              <h1>Tracer Study Fakultas Ilmu Komputer</h1>
+              <h1>UPNVJ</h1>
+            </div>
+          </div>
+          <h1>Tentang Tracer Study FIK UPNVJ</h1>
+          <p>Kelas B S1 Sistem Informasi UPNVJ terdiri dari 40 mahasiswa dengan semangat untuk terus berkembang di dunia
+            teknologi informasi. Visi kami adalah menjadi kelas terdepan dalam penerapan teknologi untuk masyarakat. Misi
+            kami adalah membangun lingkungan belajar yang kolaboratif dan inovatif.</p>
+        </div>
+      </div>
+    </section>
+
+  <special-footer></special-footer>
+  <script src="js/headerFooter.js"></script>
+</body>
+
+</html>
