fbe2b0b273b8cf210616bff2176ca6bdca4c8c84
Yoga <yogaramasahdan@gmail.com>
2024-12-09 06:41:55 +0700

    event ke 3 nya semua nya udahan ya bob

--- resources/views/career-class.blade.php
+++ resources/views/career-class.blade.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,69 +17,107 @@
 
 <body>
 <special-header></special-header>
-    <main class="flex flex-col m-10">
-        <section class="border-b-2 border-black py-5">
-            <div class="grid grid-cols-2 gap-10">
-                <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/12/IMG_9349-2048x1365.jpg"
-                    alt="" class="object-cover h-[300px] w-full">
-                <div class="flex flex-col gap-3">
-                    <h1 class="text-2xl font-bold">Kuliah Tamu Prodi Sistem Informasi UPNVJ: Kupas Tuntas Supply Chain Management di Era Digital"</h1>
-                    <p class="text-neutral-400">29 November 2024</p>
-                    <p>Program Studi Sistem Informasi Program Sarjana Fakultas Ilmu Komputer Universitas Pembangunan Nasional Veteran Jakarta (UPNVJ) sukses mengadakan kuliah tamu bertema “Supply Chain Management di Era Digital”. Acara ini menghadirkan narasumber utama, Winoto Agniawan Senoadji, MPM, CLC, CHRA, AAPM, CISPO, CITSM, seorang Digital Transformation Leader dari PT East West Seed Indonesia.
-                    </p>
+<section class="hero">
+    <div class="container">
+        <div class="home">
+            <section class="event">
+                <div class="container">
+                    <div class="grid-2">
+                        <div class="card">
+                            <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/12/IMG_9349-2048x1365.jpg" alt="">
+                            </div>
+                        <div class="card">
+                            <h1>Kuliah Tamu Prodi Sistem Informasi UPNVJ: Kupas Tuntas Supply Chain Management di Era Digital</h1>
+                            <h2>29 November 2024</h2>
+                            <p>Program Studi Sistem Informasi Program Sarjana Fakultas Ilmu Komputer Universitas Pembangunan Nasional Veteran Jakarta (UPNVJ) sukses mengadakan kuliah tamu bertema “Supply Chain Management di Era Digital”. Acara ini menghadirkan narasumber utama, Winoto Agniawan Senoadji, MPM, CLC, CHRA, AAPM, CISPO, CITSM, seorang Digital Transformation Leader dari PT East West Seed Indonesia.</p>
+                        </div>
+                    </div>
+                    <div class="border">
+                    </div>
+                    <div class="grid-2">
+                        <div class="card">
+                            <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/12/IMG_9211-1536x1024.jpg" alt="">
+                            </div>
+                        <div class="card">
+                            <h1>Kuliah Tamu Sains Data UPNVJ: Matematika Diskrit dan Society 5.0 Bersama UniSZA Malaysia</h1>
+                            <h2>26 November 2024</h2>
+                            <p>Program Studi Sains Data, Fakultas Ilmu Komputer, Universitas Pembangunan Nasional Veteran Jakarta (UPNVJ), sukses mengadakan kuliah tamu bertema “Discrete Mathematics and Its Implementation in Society 5.0”. Acara yang digelar di Laboratorium Fakultas Ilmu Komputer, lantai 4 Gedung Ki Hajar Dewantara ini menghadirkan Dr. Siti Sabariah binti Abas, dosen dari Universiti Sultan Zainal Abidin (UniSZA), Malaysia, sebagai pembicara utama.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="border">
+                    </div>
+                    <div class="grid-2">
+                        <div class="card">
+                            <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/11/Day-1-1.jpeg" alt="">
+                            </div>
+                        <div class="card">
+                            <h1>FIK UPNVJ Dorong Publikasi Internasional melalui Pelatihan Penulisan Artikel Terindeks Scopus</h1>
+                            <h2>26 November 2024</h2>
+                            <p>Fakultas Ilmu Komputer Universitas Pembangunan Nasional “Veteran” Jakarta sukses menyelenggarakan pelatihan intensif bertajuk “Penulisan Artikel Ilmiah Terindeks Scopus” selama tiga hari, mulai tanggal 10 hingga 12 November 2024 di Park 5 Hotel. Kegiatan ini diikuti oleh dosen Fakultas Ilmu Komputer dengan tujuan meningkatkan keterampilan menulis ilmiah dan mendorong publikasi di jurnal bereputasi internasional.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="border">
+                    </div>
+                    <div class="grid-2">
+                        <div class="card">
+                            <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/11/android2.jpg" alt="">
+                            </div>
+                        <div class="card">
+                            <h1>Pelaksanaan Acara Seminar & Workshop Learn With Android dari KSM Android</h1>
+                            <h2>16 November 2024</h2>
+                            <p>Pada hari Sabtu, 16 November 2024, telah diselenggarakan acara Seminar & Workshop Learn With Android oleh KSM Android Fakultas Ilmu Komputer UPN “Veteran” Jakarta periode 2024 dengan tema “Insight Revolution : Turning Data Into Innovation”. Acara ini merupakan program kerja dari Divisi Akademik KSM Android, yang bertujuan untuk meningkatkan pengetahuan dan keterampilan peserta melalui kegiatan belajar yang interaktif dalam bidang Data, khususnya pada Data Analytics. Acara ini dilaksanakan secara Luring (luar jaringan) yang berlokasi di Auditorium Bhinneka Tunggal Ika UPNVJ dan dihadiri oleh 122 peserta mahasiswa aktif dari berbagai Fakultas dan Universitas.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="border">
+                    </div>
+                    <div class="grid-2">
+                        <div class="card">
+                            <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/11/webinar5.png" alt="">
+                            </div>
+                        <div class="card">
+                            <h1>Berita Acara Webinar Program Intensive Learning (POINTER) 2024</h1>
+                            <h2>9 November 2024</h2>
+                            <p>Pada Sabtu, 9 November 2024, Badan Eksekutif Mahasiswa Fakultas Ilmu Komputer UPN “Veteran” Jakarta mengadakan Webinar Program Intensive Learning (POINTER) 2024 dengan tema “From Zero to Hero: Starting Your Journey in Computer Competition.” Acara daring ini bertujuan membekali mahasiswa dalam strategi kompetisi di bidang Data Science dan Software Development, diikuti oleh 272 peserta dari berbagai program studi. Webinar dimulai dengan pembukaan dan menyanyikan lagu kebangsaan, dilanjutkan sambutan dari Ketua Pelaksana, Ananda Ganapati Febrian, dan Ketua BEM, Ranadea Muhammad Gunawan, serta sesi dokumentasi peserta.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="border">
+                    </div>
+                    <div class="grid-2">
+                        <div class="card">
+                            <img src="" alt="">
+                            </div>
+                        <div class="card">
+                            <h1></h1>
+                            <h2></h2>
+                            <p>
+                        </div>
+                    </div>
+                    <div class="border">
+                    </div>
+                    <div class="grid-2">
+                        <div class="card">
+                            <img src="" alt="">
+                            </div>
+                        <div class="card">
+                            <h1></h1>
+                            <h2></h2>
+                            <p>
+                        </div>
+                    </div>
+                    <div class="border">
+                    </div>
                 </div>
-            </div>
-        </section>
-        <section class="border-b-2 border-black py-5">
-            <div class="grid grid-cols-2 gap-10">
-                <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/12/IMG_9211-1536x1024.jpg"
-                    alt="" class="object-cover h-[300px] w-full">
-                <div class="flex flex-col gap-3">
-                    <h1 class="text-2xl font-bold">Kuliah Tamu Sains Data UPNVJ: Matematika Diskrit dan Society 5.0 Bersama UniSZA Malaysia</h1>
-                    <p class="text-neutral-400">26 November 2024</p>
-                    <p>Program Studi Sains Data, Fakultas Ilmu Komputer, Universitas Pembangunan Nasional Veteran Jakarta (UPNVJ), sukses mengadakan kuliah tamu bertema “Discrete Mathematics and Its Implementation in Society 5.0”. Acara yang digelar di Laboratorium Fakultas Ilmu Komputer, lantai 4 Gedung Ki Hajar Dewantara ini menghadirkan Dr. Siti Sabariah binti Abas, dosen dari Universiti Sultan Zainal Abidin (UniSZA), Malaysia, sebagai pembicara utama.
-                    </p>
-                </div>
-            </div>
-        </section>
-        <section class="border-b-2 border-black py-5">
-            <div class="grid grid-cols-2 gap-10">
-                <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/11/Day-1-1.jpeg"
-                    alt="" class="object-cover h-[300px] w-full">
-                <div class="flex flex-col gap-3">
-                    <h1 class="text-2xl font-bold">FIK UPNVJ Dorong Publikasi Internasional melalui Pelatihan Penulisan Artikel Terindeks Scopus</h1>
-                    <p class="text-neutral-400">26 November 2024</p>
-                    <p>Fakultas Ilmu Komputer Universitas Pembangunan Nasional “Veteran” Jakarta sukses menyelenggarakan pelatihan intensif bertajuk “Penulisan Artikel Ilmiah Terindeks Scopus” selama tiga hari, mulai tanggal 10 hingga 12 November 2024 di Park 5 Hotel. Kegiatan ini diikuti oleh dosen Fakultas Ilmu Komputer dengan tujuan meningkatkan keterampilan menulis ilmiah dan mendorong publikasi di jurnal bereputasi internasional.
-                    </p>
-                </div>
-            </div>
-        </section>
-        <section class="border-b-2 border-black py-5">
-            <div class="grid grid-cols-2 gap-10">
-                <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/11/android2.jpg"
-                    alt="" class="object-cover h-[350px] w-full">
-                <div class="flex flex-col gap-3">
-                    <h1 class="text-2xl font-bold">Pelaksanaan Acara Seminar & Workshop Learn With Android dari KSM Android</h1>
-                    <p class="text-neutral-400">16 November 2024</p>
-                    <p>Pada hari Sabtu, 16 November 2024, telah diselenggarakan acara Seminar & Workshop Learn With Android oleh KSM Android Fakultas Ilmu Komputer UPN “Veteran” Jakarta periode 2024 dengan tema “Insight Revolution : Turning Data Into Innovation”. Acara ini merupakan program kerja dari Divisi Akademik KSM Android, yang bertujuan untuk meningkatkan pengetahuan dan keterampilan peserta melalui kegiatan belajar yang interaktif dalam bidang Data, khususnya pada Data Analytics. Acara ini dilaksanakan secara Luring (luar jaringan) yang berlokasi di Auditorium Bhinneka Tunggal Ika UPNVJ dan dihadiri oleh 122 peserta mahasiswa aktif dari berbagai Fakultas dan Universitas.
-                    </p>
-                </div>
-            </div>
-        </section>
-        <section class="border-b-2 border-black py-5">
-            <div class="grid grid-cols-2 gap-10">
-                <img src="https://new-fik.upnvj.ac.id/wp-content/uploads/2024/11/webinar5.png"
-                    alt="" class="object-cover h-[500px] w-full">
-                <div class="flex flex-col gap-3">
-                    <h1 class="text-2xl font-bold">Berita Acara Webinar Program Intensive Learning (POINTER) 2024</h1>
-                    <p class="text-neutral-400">9 November 2024</p>
-                    <p>Pada Sabtu, 9 November 2024, Badan Eksekutif Mahasiswa Fakultas Ilmu Komputer UPN “Veteran” Jakarta mengadakan Webinar Program Intensive Learning (POINTER) 2024 dengan tema “From Zero to Hero: Starting Your Journey in Computer Competition.” Acara daring ini bertujuan membekali mahasiswa dalam strategi kompetisi di bidang Data Science dan Software Development, diikuti oleh 272 peserta dari berbagai program studi. Webinar dimulai dengan pembukaan dan menyanyikan lagu kebangsaan, dilanjutkan sambutan dari Ketua Pelaksana, Ananda Ganapati Febrian, dan Ketua BEM, Ranadea Muhammad Gunawan, serta sesi dokumentasi peserta.
-                    </p>
-                </div>
-            </div>
-        </section>
-    </main>
-    <special-footer></special-footer>
-</body>
+                
+            </section>
 
+        </div>
+    </div>
+</section>
+
+<special-footer></special-footer>
+</body>
 </html>
